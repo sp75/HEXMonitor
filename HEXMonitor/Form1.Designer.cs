1eac66ec1d1c51327ffb56028e08c8a49fd2ec11
sp75 <palamarchuk-sv@mail.ru>
2017-09-29 01:39:12 -0400

    ff

---
+++ HEXMonitor/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace HEXMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LastDays = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 132);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(341, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Last Run";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Satrt Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Copy To";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Last ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(338, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = " days";
+            // 
+            // LastDays
+            // 
+            this.LastDays.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::HEXMonitor.Properties.Settings.Default, "days", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.LastDays.Location = new System.Drawing.Point(281, 15);
+            this.LastDays.Name = "LastDays";
+            this.LastDays.Size = new System.Drawing.Size(51, 20);
+            this.LastDays.TabIndex = 12;
+            this.LastDays.Text = global::HEXMonitor.Properties.Settings.Default.days;
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::HEXMonitor.Properties.Settings.Default, "path", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox1.Location = new System.Drawing.Point(82, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = global::HEXMonitor.Properties.Settings.Default.path;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm";
+            this.dateTimePicker1.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::HEXMonitor.Properties.Settings.Default, "dt", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(82, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(134, 26);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Value = global::HEXMonitor.Properties.Settings.Default.dt;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 167);
+            this.Controls.Add(this.LastDays);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "HEX Monitor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox LastDays;
+    }
+}
+
---
+++ HEXMonitor/Form1.cs
@@ -0,0 +1,365 @@
+﻿using Labdaq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using FormulaExcel;
+using DocumentFormat.OpenXml.ReportBuilder;
+
+namespace HEXMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      //      dateTimePicker1.CustomFormat = "hh:mm";
+
+       //     dateTimePicker1.Text = dateTimePicker1.Value.ToString();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            progressBar1.Visible = true;
+            var hl7_patch = textBox1.Text;
+            var dt = Dt();
+
+            string sql_orders = @"SELECT 
+                    rq.ACC_ID,        
+                    rq.PAT_ID,
+                    rq.CREATED_DATE,
+                    rq.DRAW_DATE,   
+                    rq.RECEIVED_DATE,
+                    rq.EXTERNAL_NO, 
+                    rq.DIAGNOSIS_CODE1, 
+                    rq.PI_ID1,
+                    rq.PI_ID2,
+                    rq.PI_ID3,
+                    p.F_NAME,
+                    p.L_NAME,
+                    p.M_NAME ,
+                    p.BIRTH,
+                    p.GENDER,
+                    p.ADDRESS1,
+                    p.ADDRESS2,
+                    p.CITY,
+                    p.STATE,
+                    p.ZIP,
+                    p.SSN,
+                    p.H_PHONE,
+                    p.MARITAL_STATUS,
+                    d.DOC_ID,
+                    d.F_NAME as D_F_NAME,
+                    d.L_NAME as D_L_NAME,
+                    d.M_NAME as D_M_NAME,
+                    d.ADDRESS1 as D_ADDRESS1
+
+                FROM requisitions rq  
+                inner join PATIENTS p on rq.PAT_ID = p.PAT_ID
+                inner join  DOCTORS d on rq.DOC_ID1 = d.DOC_ID
+                
+                WHERE rq.DEL_FLAG='F' and rq.CREATED_DATE BETWEEN SYSDATE - {0} and SYSDATE 
+                and ( exists (select rp.rp_id from RL_REQ_PANELS rp, RL_RESULTS res where rp.rp_id = res.rp_id and  rq.acc_id = rp.acc_id and res.STATUS='F' )
+                     or exists (select rp.rp_id from REQ_PANELS rp, RESULTS res where rp.rp_id = res.rp_id and  rq.acc_id = rp.acc_id  )
+                    ) 
+                order by rq.PAT_ID ";
+
+            string sql_panels = @"  select rp.RP_ID, rp.PROFILE_NAME as PANEL_NAME,rp.PROFILE_ID PANEL_ID, rp.CREATED_DATE, min(cpt.CPT_ID) CPT_ID, count(res.rp_id ) count_test
+                from RL_REQ_PANELS rp
+                inner join RL_RESULTS res on res.rp_id = rp.rp_id and res.DEL_FLAG='F'
+                left outer join RL_REQ_PANEL_CPTS cpt on rp.RP_ID = cpt.RP_ID
+                where rp.DEL_FLAG='F' and rp.ACC_ID  = {0} 
+                group by rp.RP_ID, rp.PROFILE_NAME,rp.PROFILE_ID, rp.CREATED_DATE
+             
+                union all
+             
+                select rp.RP_ID, p.PANEL_NAME, TO_CHAR( rp.PANEL_ID) PANEL_ID, rp.CREATED_DATE, min(cpt.CPT_ID) CPT_ID, count(res.rp_id ) count_test
+                from REQ_PANELS rp
+                inner join PANELS p on rp.PANEL_ID = p.PANEL_ID
+                inner join RESULTS res on  res.rp_id = rp.rp_id and res.DEL_FLAG='F'
+                left outer join REQ_PANEL_CPTS cpt on rp.RP_ID = cpt.RP_ID
+                where rp.DEL_FLAG='F' and rp.ACC_ID  = {0} 
+                group by rp.RP_ID, p.PANEL_NAME,rp.PANEL_ID, rp.CREATED_DATE";
+
+            int msg_count = 0;
+
+            using (var lab = new LabdaqClient())
+            {
+                int step = 0;
+                var d = DateTime.Now;
+                List<Dictionary<string, object>> Req = lab.RunSql(String.Format(sql_orders, LastDays.Text), -1);
+                int count_req = Req.Count();
+
+                using (var db = new LENCO_WEBEntities())
+                {
+                    foreach (Dictionary<string, object> row in Req)
+                    {
+                        string ACC_ID = row["ACC_ID"].ToString();
+                        string hl7_str = "";
+
+                        long acc_id = Convert.ToInt32(row["ACC_ID"]);
+
+                        var req = new HEX_Requisitions()
+                         {
+                             acc_id = acc_id,
+                             date_processed = d,
+                             patient_first_name = row["F_NAME"].ToString(),
+                             patient_last_name = row["L_NAME"].ToString(),
+                             requisition_date = Convert.ToDateTime(row["CREATED_DATE"].ToString())
+                         };
+
+                        int count = 0;
+                        StringBuilder sb_pan = new StringBuilder();
+                        List<Dictionary<string, object>> panels = lab.RunSql(String.Format(sql_panels, row["ACC_ID"]), -1);
+
+                        if (panels.Any(a => a["PANEL_ID"].ToString() == "580") && panels.Any(a => a["PANEL_ID"].ToString() == "6700"))
+                        {
+                            panels.Remove(panels.FirstOrDefault(a => a["PANEL_ID"].ToString() == "580"));
+                        }
+                        if (panels.Any(a => a["PANEL_ID"].ToString() == "745") && panels.Any(a => a["PANEL_ID"].ToString() == "6700"))
+                        {
+                            panels.Remove(panels.FirstOrDefault(a => a["PANEL_ID"].ToString() == "745"));
+                        }
+
+                        foreach (Dictionary<string, object> p in panels)
+                        {
+                            int rp_id = Convert.ToInt32(p["RP_ID"]);
+                            if (!db.HEX_Panels.Any(a => a.rp_id == rp_id))
+                            {
+                                req.HEX_Panels.Add(new HEX_Panels()
+                                {
+                                    acc_id = acc_id,
+                                    panel_name = p["PANEL_NAME"].ToString(),
+                                    rp_id = rp_id,
+                                    panel_id = p["PANEL_ID"].ToString()
+                                });
+
+                                ++count;
+                                sb_pan.Append(HL7(count, row, p));
+                            }
+                        }
+
+                        if (count > 0)
+                        {
+                            //  sw.Write(HL7(lab, row));
+                            //  sw.Write(sb_pan.ToString());
+                            hl7_str += HL7(lab, row);
+                            hl7_str += sb_pan.ToString();
+
+                            req.hl7_data = hl7_str;
+
+                            try
+                            {
+                                if (!String.IsNullOrEmpty(hl7_str))
+                                {
+                                    string fname = Path.Combine(hl7_patch, ACC_ID + ".hl7");
+                                    StreamWriter sw = new StreamWriter(fname, true);
+                                    sw.Write(hl7_str);
+                                    sw.Close();
+                                }
+
+                                db.HEX_Requisitions.Add(req);
+                                db.SaveChanges();
+
+                                AddRow(dt, row);
+
+                                ++msg_count;
+                            }
+                            catch
+                            {
+                                ;
+                            }
+                        }
+
+                        Application.DoEvents();
+                        progressBar1.Value = ++step * 100 / count_req;
+                    }
+                }
+
+                label2.Text = d.ToString();
+            }
+
+            button1.Enabled = true;
+            progressBar1.Visible = false;
+
+
+            SendEmail("Requisitions count - " + msg_count.ToString(), BuildRep(dt));
+        }
+
+        private void SendEmail(String Body, String attachmentFilename)
+        {
+         //   String name = Path.GetFileName(attachmentFilename);
+          //  ZipFile.CreateFromDirectory(Path.GetDirectoryName(attachmentFilename), @"arh\" + name + ".zip");
+          //  File.Delete(attachmentFilename);
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("mail.authsmtp.com");
+
+                mail.From = new MailAddress("info@lencolab.com");
+            //    mail.To.Add("sergeyp@4ib.com"); 
+                mail.To.Add("felix@lencolab.com"); 
+                mail.Subject = "HEX Monitor";
+                mail.CC.Add("sergeyp@4ib.com");
+                mail.Body = Body;
+
+                SmtpServer.Port = 2525;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("ac60691", "fargsga8ptbnun");
+               // SmtpServer.EnableSsl = true;
+
+               if (attachmentFilename != null)
+                {
+                    mail.Attachments.Add(new Attachment(attachmentFilename));
+                }
+
+                SmtpServer.Send(mail);
+              
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
+        }
+
+        private string BuildRep(DataTable dt)
+        {
+            string output_file = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Rep", DateTime.Now.ToLongDateString() +"_"+ DateTime.Now.Ticks.ToString() + ".xlsx");
+            var ds = new DataSet();
+            ds.Tables.Add(dt );
+
+            CalculationlFormulaExcel.CalcSpreadsheetDocument(
+                ReportBuilderXLS.GenerateReport(
+                    ds,
+                    Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Template", "HexLog.xlsx")),
+                true, output_file);
+
+            return output_file;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var dt = DateTime.Now;
+            if (dt.Minute == dateTimePicker1.Value.Minute && dt.Hour == dateTimePicker1.Value.Hour && dt.Second == dateTimePicker1.Value.Second)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private String HL7(LabdaqClient lc, Dictionary<string, object> pat)
+        {
+            var dt = DateTime.Now;
+            StringBuilder sb = new StringBuilder();
+
+            string birth = String.IsNullOrEmpty(pat["BIRTH"].ToString()) ? "" : DateTime.Parse(pat["BIRTH"].ToString()).ToString("yyyyMMdd");
+
+            sb.AppendLine(String.Format(@"MSH|^~\&|LABDAQ||||{0}||ORU^R01|{1}|P|2.4|||||||||", dt.ToString("yyyyMMddHHmm"), dt.ToString("yyyyMMddHHmmssfff")));
+            sb.AppendLine(String.Format(@"PID|1||{0}||{1}^{2}^{3}^||{4}|{5}|||{6}^{7}^{8}^{9}^{10}||{11}|||{12}|||{13}|||||||||||||||||",
+                pat["PAT_ID"], pat["L_NAME"], pat["F_NAME"], pat["M_NAME"], birth, pat["GENDER"], pat["ADDRESS1"], pat["ADDRESS2"], pat["CITY"], 
+                pat["STATE"], pat["ZIP"], pat["H_PHONE"].ToString().Replace("(", "").Replace(")", "").Replace("-", ""), pat["MARITAL_STATUS"], pat["SSN"].ToString().Replace("-","")));
+
+            int idx = 0;
+            foreach (var item in GetIns(lc, pat))
+            {
+                string EFFECTIVE_DATE = String.IsNullOrEmpty(item["EFFECTIVE_DATE"].ToString()) ? "" : DateTime.Parse(item["EFFECTIVE_DATE"].ToString()).ToString("yyyyMMdd");
+
+                sb.AppendLine(String.Format(@"IN1|{0}||{1}^|{2}|^^^^| ||||||{14}|||Z|{3}^{4}^{5}||{6}|{7}^{8}^{9}^{10}^{11}|||{12}||||||||||||||{13}|||||||F||||",
+                    ++idx, item["INS_ID"], item["INS_NAME"], item["L_NAME"], item["F_NAME"], item["M_NAME"], birth, item["ADDRESS1"], item["ADDRESS2"], item["CITY"], item["STATE"], item["ZIP"], item["PRIORITY"], item["POLICY_NO"], EFFECTIVE_DATE));
+
+                sb.AppendLine(String.Format(@"IN2||{0}|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||{1}|",pat["SSN"].ToString().Replace("-",""),  pat["H_PHONE"].ToString().Replace("(", "").Replace(")", "").Replace("-", "")));
+            }
+
+            return sb.ToString();
+        }
+
+        private String HL7(int obr, Dictionary<string, object> pat, Dictionary<string, object> panel)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string draw_date = String.IsNullOrEmpty(pat["DRAW_DATE"].ToString()) ? "" : DateTime.Parse(pat["DRAW_DATE"].ToString()).ToString("yyyyMMddHHmm"); 
+            string received_date =  String.IsNullOrEmpty(pat["RECEIVED_DATE"].ToString()) ? "" :  DateTime.Parse(pat["RECEIVED_DATE"].ToString()).ToString("yyyyMMddHHmm");
+
+            sb.AppendLine(String.Format(@"ORC|RE|{0}^LABDAQ|{1}||||||{2}|||{3}^{4}^{5} ^{6}^^^^P||||^|", pat["ACC_ID"], pat["EXTERNAL_NO"], DateTime.Parse(pat["CREATED_DATE"].ToString()).ToString("yyyyMMddHHmm"),
+                    pat["DOC_ID"], pat["D_L_NAME"], pat["D_F_NAME"], pat["D_ADDRESS1"]));
+
+            sb.AppendLine(String.Format(@"OBR|{0}|{1}|{2}|{3}^{4}^L^{11}^^CPT|||{5}|||~AV|A|||{6}||{7}^{8}^{9}^{10}^^^^P|||||||||F||1^^^^^R^^^||||||||||", obr, pat["ACC_ID"], pat["EXTERNAL_NO"], panel["PANEL_ID"], 
+                panel["PANEL_NAME"], draw_date, received_date, pat["DOC_ID"], pat["D_L_NAME"], pat["D_F_NAME"], pat["D_ADDRESS1"], panel["CPT_ID"]));
+
+            if (!String.IsNullOrEmpty(Convert.ToString(pat["DIAGNOSIS_CODE1"])))
+            {
+                sb.AppendLine(String.Format(@"DG1|{0}|I10|{1}|{2}|{3}||||||||||", obr, pat["DIAGNOSIS_CODE1"], "", DateTime.Parse(panel["CREATED_DATE"].ToString()).ToString("yyyyMMddHHmm")));
+            }
+
+            return sb.ToString();
+        }
+
+        private List<Dictionary<string, object>> GetIns(LabdaqClient lc, Dictionary<string, object> pat)
+        {
+            string sql = @"select p.*, ins.INS_NAME  from PAT_INSURANCE p
+                           inner join INS_COMPANIES ins ON p.INS_ID = ins.INS_ID 
+                           where p.PAT_ID  = '{0}' and ( PI_ID = {1} or PI_ID = {2} or PI_ID = {3})
+                           order by p.PRIORITY";
+
+            sql = String.Format(sql, pat["PAT_ID"], !String.IsNullOrEmpty(pat["PI_ID1"].ToString()) ? pat["PI_ID1"] : -1, !String.IsNullOrEmpty(pat["PI_ID2"].ToString()) ? pat["PI_ID2"] : -1, !String.IsNullOrEmpty(pat["PI_ID3"].ToString()) ? pat["PI_ID3"] : -1);
+
+            return lc.RunSql(sql, -1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fDialog = new FolderBrowserDialog();
+            fDialog.SelectedPath = textBox1.Text;
+            fDialog.ShowDialog();
+            textBox1.Text = fDialog.SelectedPath;
+        }
+
+        private DataTable Dt()
+        {
+            var dt = new DataTable("range1");
+            dt.Columns.Add("ACC_ID");
+            dt.Columns.Add("CREATED_DATE");
+            dt.Columns.Add("F_NAME");
+            dt.Columns.Add("L_NAME");
+            dt.Columns.Add("BIRTH");
+            dt.Columns.Add("D_F_NAME");
+            dt.Columns.Add("D_L_NAME");
+            dt.Columns.Add("D_ADDRESS1");
+            return dt;
+        }
+
+        private void AddRow(DataTable dt, Dictionary<string, object> row)
+        {
+            var r = dt.NewRow();
+            r["ACC_ID"] = row["ACC_ID"];
+            r["CREATED_DATE"] = row["CREATED_DATE"];
+            r["F_NAME"] = row["F_NAME"];
+            r["L_NAME"] = row["L_NAME"];
+            r["BIRTH"] = row["BIRTH"];
+            r["D_F_NAME"] = row["D_F_NAME"];
+            r["D_L_NAME"] = row["D_L_NAME"];
+            r["D_ADDRESS1"] = row["D_ADDRESS1"];
+            dt.Rows.Add(r);
+
+            dt.AcceptChanges();
+        }
+
+     }
+}
